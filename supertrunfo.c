faf986efb9e7b00c45b533b53783ac12e7de1ba1
Viniciusndx <vinicius82430175@gmail.com>
2025-08-05 12:26:14 +0000

    criei as variaveis

---
+++ supertrunfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//criando o super trunfo - novato
+
+int main() { 
+    char estado;
+    char codigo[4];
+    char nome[20];
+    int populacao;
+    float àrea;
+    float PIB;
+    int turisticos;
+
+    printf ("Super Trunfo!\n");
+    printf( "Chegou a hora de cadastrar as informaçoes.\n Preparados?!\n");
+
+}
