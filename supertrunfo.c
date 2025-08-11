1e48aa1bc397f8573a733b053d834def302f954a
Viniciusndx <vinicius82430175@gmail.com>
2025-08-11 12:56:04 +0000

    criei a primeira carta( esta bugado)

--- supertrunfo.c
+++ supertrunfo.c
@@ -11,6 +11,40 @@ int main() {
     int turisticos;
 
     printf ("Super Trunfo!\n");
-    printf( "Chegou a hora de cadastrar as informaçoes.\n Preparados?!\n");
+    printf( "Chegou a hora de cadastrar as informaçoes.\nPreparados?!\n");
+
+    //desenvolvendo as cartas
+
+    printf("digite o nome: \n");
+    scanf("%s", &nome);
+    
+    printf("digite o codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("digite a área: \n");
+    scanf("%f", &àrea);
+
+    printf("digite a população: \n");
+    scanf("%d", &populacao);
+    
+    printf("digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("digite o numero de pontos turisticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("digite o estado: \n");
+    scanf("%c", &estado);
+
+    printf("Aqui está a primeira carta: \n");
+    printf("O estado é: %c\n", estado);
+    printf("O codigo é: %s\n", codigo);
+    printf("O nome é: %s\n", nome);
+    printf("A população é: %d\n", populacao);
+    printf("A àrea é: %f\n", àrea);
+    printf("O PIB é: %2.f\n", PIB);
+    printf("Os pontos turisticos é: %d\n", turisticos);
+
+    
 
 }
