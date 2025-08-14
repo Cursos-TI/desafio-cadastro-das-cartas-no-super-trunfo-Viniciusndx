0914cbaa9e73af83e68c465502b8cc8a5429e4cd
Viniciusndx <vinicius82430175@gmail.com>
2025-08-14 23:10:16 +0000

    criado as cartas

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,21 +2,24 @@
 //criando o super trunfo - novato
 
 int main() { 
-    char estado;
+    char estado[20];
     char codigo[4];
     char nome[20];
     int populacao;
     float àrea;
-    float PIB;
+    float Pib;
     int turisticos;
 
     printf ("Super Trunfo!\n");
-    printf( "Chegou a hora de cadastrar as informaçoes.\nPreparados?!\n");
+    printf( "Chegou a hora de cadastrar as informaçoes da primeira carta!!\nPreparados?!\n\n");
 
     //desenvolvendo as cartas
 
     printf("digite o nome: \n");
     scanf("%s", &nome);
+
+    printf("digite o estado: \n");
+    scanf("%s", &estado);
     
     printf("digite o codigo: \n");
     scanf("%s", &codigo);
@@ -28,21 +31,20 @@ int main() {
     scanf("%d", &populacao);
     
     printf("digite o PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &Pib);
 
     printf("digite o numero de pontos turisticos: \n");
     scanf("%d", &turisticos);
 
-    printf("digite o estado: \n");
-    scanf("%c", &estado);
 
-    printf("Aqui está a primeira carta: \n");
+    printf("Aqui está a primeira carta: \n\n");
+    printf("Carta 1");
     printf("O estado é: %c\n", estado);
-    printf("O codigo é: %s\n", codigo);
-    printf("O nome é: %s\n", nome);
-    printf("A população é: %d\n", populacao);
-    printf("A àrea é: %f\n", àrea);
-    printf("O PIB é: %2.f\n", PIB);
+    printf("O codigo da carta é: %s\n", codigo);
+    printf("A cidade é: %s\n", nome);
+    printf("O numero populacional é: %d\n", populacao);
+    printf("A àrea é de: %2.f\n Km²", àrea);
+    printf("O PIB é: %2f\n Bilhoes de reais", Pib);
     printf("Os pontos turisticos é: %d\n", turisticos);
 
     
