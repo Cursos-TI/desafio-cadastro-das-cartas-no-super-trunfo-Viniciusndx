43357c4b93cd07b7d39a3c61cd2a6b5b681ebd58
Viniciusndx <vinicius82430175@gmail.com>
2025-08-25 12:53:16 +0000

    tenho que arrumar alguma coisa

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,13 +2,15 @@
 //criando o super trunfo - novato
 
 int main() { 
-    char estado[20];
-    char codigo[4];
-    char nome[20];
+    char estado[];
+    char codigo[];
+    char nome[];
     int populacao;
     float àrea;
     float Pib;
     int turisticos;
+    float densidade;
+    float capita;
 
     printf ("Super Trunfo!\n");
     printf( "Chegou a hora de cadastrar as informaçoes da primeira carta!!\nPreparados?!\n\n");
@@ -36,16 +38,20 @@ int main() {
     printf("digite o numero de pontos turisticos: \n");
     scanf("%d", &turisticos);
 
+    &densidade = populacao / àrea
+    &capita = Pib / populacao
 
     printf("Aqui está a primeira carta: \n\n");
-    printf("Carta 1");
-    printf("O estado é: %c\n", estado);
+    printf("Carta 1\n");
+    printf("O estado é: %s\n", estado);
     printf("O codigo da carta é: %s\n", codigo);
     printf("A cidade é: %s\n", nome);
     printf("O numero populacional é: %d\n", populacao);
-    printf("A àrea é de: %2.f\n Km²", àrea);
-    printf("O PIB é: %2f\n Bilhoes de reais", Pib);
-    printf("Os pontos turisticos é: %d\n", turisticos);
+    printf("A àrea é de: %2.f Km²\n", àrea);
+    printf("O PIB é: %2.f Bilhoes de reais\n", Pib);
+    printf("Os pontos turisticos é: %d\n\n", turisticos);
+    printf("A densidade populacional é: %2.f\n", densidade);
+    printf("O PIB per capita é: %2.f\n", capita);
 
 
     printf( "Chegou a hora de cadastrar as informaçoes da segunda carta!!\nPreparados?!\n\n");
@@ -75,12 +81,12 @@ int main() {
 
     printf("Aqui está a segunda carta: \n\n");
     printf("Carta 2");
-    printf("O estado é: %c\n", estado);
+    printf("O estado é: %s\n", estado);
     printf("O codigo da carta é: %s\n", codigo);
     printf("A cidade é: %s\n", nome);
     printf("O numero populacional é: %d\n", populacao);
-    printf("A àrea é de: %2.f\n Km²", àrea);
-    printf("O PIB é: %2f\n Bilhoes de reais", Pib);
+    printf("A àrea é de: %2.f Km²\n", àrea);
+    printf("O PIB é: %2f Bilhoes de reais\n", Pib);
     printf("Os pontos turisticos é: %d\n", turisticos);
 
     
